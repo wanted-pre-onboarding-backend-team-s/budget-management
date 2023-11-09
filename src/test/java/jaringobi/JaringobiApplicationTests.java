5f0381b863a703803529bc9869a49cac556ec1f9
kmss6905 <kmss69052@naver.com>
2023-11-10 00:52:11 +0900

    Init: 프로젝트 세팅

---
+++ src/test/java/jaringobi/JaringobiApplicationTests.java
@@ -0,0 +1,13 @@
+package jaringobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaringobiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
