b134c88fcdc76754de979c623ed33a8683535683
kmss6905 <kmss69052@naver.com>
2023-11-10 18:40:43 +0900

    Feature: 암호화 인코더 빈 설정 - Argon2

---
+++ src/main/java/jaringobi/common/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package jaringobi.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+}
