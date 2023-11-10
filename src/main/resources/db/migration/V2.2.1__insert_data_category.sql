6eaa4860fae35a72c26ead21844d580f2d4c7392
kmss6905 <kmss69052@naver.com>
2023-11-11 02:19:53 +0900

    Feature: category ddl, dml추가

---
+++ src/main/resources/db/migration/V2.2.1__insert_data_category.sql
@@ -0,0 +1,31 @@
+-- 고정비
+insert into category(id, name) values (1, '월세');
+insert into category(id, name) values (2, '관리비');
+
+-- 생활비
+insert into category(id, name) values (3, '식비');
+insert into category(id, name) values (4, '병원비');
+
+-- 활동비
+insert into category(id, name) values (5, '여행');
+insert into category(id, name) values (6, '취미');
+
+-- 친목비
+insert into category(id, name) values (7, '모임비');
+insert into category(id, name) values (8, '데이트비');
+
+-- 꾸밈비
+insert into category(id, name) values (9, '의류');
+insert into category(id, name) values (10, '헤어');
+insert into category(id, name) values (11, '잡화');
+
+-- 차량비
+insert into category(id, name) values (12, '주유비');
+insert into category(id, name) values (13, '자동차보험');
+
+-- 기여비
+insert into category(id, name) values (14, '부모님 용돈');
+insert into category(id, name) values (15, '경조사비');
+
+-- 예비비
+insert into category(id, name) values (16, '예상치 못한 지출');
---
+++ src/main/resources/db/migration/V2.2__alter_category_not_autoid.sql
@@ -0,0 +1,1 @@
+ALTER TABLE category MODIFY id INT NOT NULL;
