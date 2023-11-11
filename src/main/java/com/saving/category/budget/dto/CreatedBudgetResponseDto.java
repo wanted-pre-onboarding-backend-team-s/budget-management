aca84f388b5ee04a0650a70edd80a063051640bd
newnyee <mebbun0615@gmail.com>
2023-11-12 07:30:14 +0900

    Refactor: 엔티티의 필요없는 필드 삭제
    Budget, Expense 엔티티의 userId 필드 삭제

--- src/main/java/com/saving/category/budget/dto/CreatedBudgetResponseDto.java
+++ src/main/java/com/saving/category/budget/dto/CreatedBudgetResponseDto.java
@@ -7,14 +7,12 @@ import lombok.Getter;
 public class CreatedBudgetResponseDto {
 
     private final Long budgetId;
-    private final Long userId;
     private final Long categoryId;
     private final int amount;
     private final String budgetYearMonth;
 
     public CreatedBudgetResponseDto(Budget budget) {
         this.budgetId = budget.getId();
-        this.userId = budget.getUserId();
         this.categoryId = budget.getCategoryId();
         this.amount = budget.getAmount();
         this.budgetYearMonth = budget.getBudgetYearMonth().substring(0,7);
