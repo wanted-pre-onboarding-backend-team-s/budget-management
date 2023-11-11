ed744b6941497dedd629c8cf574d2edcdef4b41f
Inwoo Hwang <hsjkdss228@naver.com>
2023-11-11 20:08:25 +0900

    Chore: 프로젝트 세팅
    Spring Framework 6.0.13

---
+++ src/main/java/com/wanted/jaringoby/JaringobyApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.jaringoby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaringobyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaringobyApplication.class, args);
+    }
+
+}
