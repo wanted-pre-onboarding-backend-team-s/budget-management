1572788ebcd4a3237f2e273c4c84c7f3da91672c
newnyee <mebbun0615@gmail.com>
2023-11-13 02:25:30 +0900

    Feature: 지출 목록 API 추가
    - 기본 읽어오기 기능 (기간 조회) 추가

--- src/main/java/com/saving/expense/domain/repository/ExpenseRepository.java
+++ src/main/java/com/saving/expense/domain/repository/ExpenseRepository.java
@@ -4,7 +4,7 @@ import com.saving.expense.domain.entity.Expense;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+public interface ExpenseRepository extends JpaRepository<Expense, Long>, ExpenseRepositoryCustom {
 
     Optional<Expense> findByIdAndCategoryId(Long id, Long categoryId);
 
