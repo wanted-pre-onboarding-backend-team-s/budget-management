5d3f8c20753e6f9d781892dde1b1c3d5471d32fe
buhee1029 <buhee1029@gmail.com>
2023-11-13 12:59:42 +0900

    Feature: 카테고리별 평균 예산 백분율 구하는 네이티브 쿼리 작성

--- src/main/java/com/wanted/bobo/budget/domain/BudgetRepository.java
+++ src/main/java/com/wanted/bobo/budget/domain/BudgetRepository.java
@@ -1,11 +1,25 @@
 package com.wanted.bobo.budget.domain;
 
+import com.wanted.bobo.budget.dto.BudgetStatByCategory;
 import com.wanted.bobo.category.Category;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
 
     boolean existsByUserIdAndCategory(Long userId, Category category);
+
     List<Budget> findAllByUserId(Long userId);
+
+    @Query(value = "SELECT "
+            + "category, "
+            + "ROUND(SUM(total) / (SELECT COUNT(DISTINCT user_id) FROM budgets)) AS percentage "
+            + "FROM ("
+            + "SELECT "
+            + "category, "
+            + "amount / SUM(amount) OVER (PARTITION BY user_id) * 100 AS total "
+            + "FROM budgets"
+            + ") AS category_percentage GROUP BY category", nativeQuery = true)
+    List<BudgetStatByCategory> findAverageBudgetPercentagesByCategory();
 }
