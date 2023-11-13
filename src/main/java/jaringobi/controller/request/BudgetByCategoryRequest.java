83f8fbca69e4246ff424396d1ee939239e602e8f
kmss6905 <kmss69052@naver.com>
2023-11-13 21:28:03 +0900

    Chore: BudgetByCategoryRequest.java Builder 추가

--- src/main/java/jaringobi/controller/request/BudgetByCategoryRequest.java
+++ src/main/java/jaringobi/controller/request/BudgetByCategoryRequest.java
@@ -3,9 +3,11 @@ package jaringobi.controller.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class BudgetByCategoryRequest {
 
