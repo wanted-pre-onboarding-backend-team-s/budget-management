6027b99242e4b14a5b2052341d8fa49be277adf5
kmss6905 <kmss69052@naver.com>
2023-11-14 21:40:59 +0900

    Test: TokenProvider 테스트

--- src/test/java/jaringobi/jwt/TokenProviderTest.java
+++ src/test/java/jaringobi/jwt/TokenProviderTest.java
@@ -1,6 +1,8 @@
 package jaringobi.jwt;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
@@ -10,6 +12,7 @@ import java.util.Date;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 @DisplayName("JWT 토큰 생성기 테스트")
 class TokenProviderTest {
@@ -101,5 +104,47 @@ class TokenProviderTest {
         Date expiresAt = decodedJWT.getExpiresAt();
 
         assertThat(expiresAt.before(new Date())).isTrue();
+    }
+
+    @Test
+    @DisplayName("요청 헤더의 Authorization 필드에 있는 값을 파싱한다.")
+    void parseAuthorizationFieldValue() {
+        String accessToken = tokenProvider.issueAccessToken(1L);
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addHeader("Authorization", "Bearer " + accessToken);
+
+        String token = tokenProvider.parseTokenFromHeader(mockHttpServletRequest);
+
+        assertThat(token).isEqualTo(accessToken);
+    }
+
+    @Test
+    @DisplayName("정상적인 토큰의 경우 유효성을 검사 시 True 반환")
+    void returnTrueValidToken() {
+        String invalidToken = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX3NlcSI6MSwidG9rZW5fdHlwZSI6IkFDQ0VTUyIsImlhdCI6MTY5OTk1NDUwMywiZXhwIjoxNjk5OTU1MTAzfQ.wVClKGLKceyTZwk8vncU-Ml9F0-UVDLoq7sLajV13G5UIiEmgYqTWIJHLbxN1uJlVQf50-5YTKGJzGZQCAg0Mg";
+        assertThat(tokenProvider.isValidToken(invalidToken)).isFalse();
+    }
+
+    @Test
+    @DisplayName("정상적인 토큰의 경우 유효성을 검사 시 False 반환")
+    void throwExceptionNotInvalidToken() {
+        String accessToken = tokenProvider.issueAccessToken(1L);
+        assertThat(tokenProvider.isValidToken(accessToken)).isTrue();
+    }
+
+    @Test
+    @DisplayName("유효한 토큰의 경우 예외를 던지지 않는다.")
+    void notThrowExceptionWhenValidToken() {
+        String accessToken = tokenProvider.issueAccessToken(1L);
+        assertThatCode(() -> tokenProvider.isValidToken(accessToken))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("Token 으로부터 'user_seq' Claim 을 가져온다.")
+    void getUserSeqFromToken() {
+        String accessToken = tokenProvider.issueAccessToken(1L);
+        Long userId = tokenProvider.parseUserId(accessToken);
+        assertThat(userId).isEqualTo(1L);
     }
 }
